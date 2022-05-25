9a10b003d5f5aa3dcb70b725d4f08dad158839b8
Jozell Rili <jozell.rili@gmail.com>
2022-05-25 08:38:23 +0800

    Updated importer and readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # customer-api
-Test project
+Fetch random users from RandomUser.Me API.
+
+## Stack
+- Lumen
+- Laravel-Doctrine
+
+## Setup
+Before you start the application, create the following file inside the root directory to store secrets.
+- Store root db password
+```
+db_root_password.txt
+```
+- Store user db password
+```
+db_root_password.txt
+```
+
+## Useful commands
+
+- To start container
+```
+docker-compose up -d
+```
+
+- To fetch user
+```
+php artisan user:fetch
+```
--- src/app/Services/Importer/ImporterService.php
+++ src/app/Services/Importer/ImporterService.php
@@ -3,22 +3,48 @@
 namespace App\Services\Importer;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\GuzzleException;
 
 class ImporterService implements ImporterInterface
 {
     private string $url = 'https://randomuser.me/api/';
 
-    public function fetchUsers(int $count, string $country)
+    /**
+     * @throws GuzzleException
+     */
+    public function fetchUsers(int $count, string $country): array
     {
         $params = [
-            'results' => $count ,
+            'results' => $count,
             'nat' => $country,
         ];
 
-        $httpClient = new Client();
-        $request = $httpClient->get($this->url . '?' .http_build_query($params));
-        $response = $request->getBody()->getContents();
+        $data = [];
+        try {
+            $httpClient = new Client();
+            $request = $httpClient->get($this->url . '?' . http_build_query($params));
+            $response = json_decode($request->getBody()->getContents(), true);
 
-        return json_decode($response);
+            $data['users'] = array_map(function ($user) {
+                return [
+                    'first_name' => $user['name']['first'],
+                    'last_name' => $user['name']['last'],
+                    'email' => $user['email'],
+                    'username' => $user['login']['username'],
+                    'password' => $user['login']['password'],
+                    'gender' => $user['gender'],
+                    'country' => $user['location']['country'],
+                    'city' => $user['location']['city'],
+                    'phone' => $user['phone'],
+                ];
+            }, $response['results']);
+        } catch (BadResponseException $e) {
+            $data = [
+                'error' => $e->getMessage(),
+            ];
+        }
+
+        return $data;
     }
 }
